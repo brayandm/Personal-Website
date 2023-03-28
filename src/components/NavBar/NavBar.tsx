2cb1d021db08072da969e427efa5e2edea33c3cb
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-28 18:34:10 +0200

    nav bar component added

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,94 @@
+'use client';
+
+import styles from './NavBar.module.css';
+import Image from 'next/image';
+
+export default function NavBar() {
+    return (
+        <header className={styles.header}>
+            <div className={styles.icon_navigation}>
+                <Image
+                    src={'/barra-de-menus.png'}
+                    alt={'menu'}
+                    width={30}
+                    height={30}
+                />
+            </div>
+            <section className={styles.wrapper}>
+                <button className={styles.main_button}>B</button>
+                <nav className={styles.navigation}>
+                    <ul>
+                        <li>
+                            <a className={styles.nav_about} href="#about-id">
+                                <p>01.</p> About
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                className={styles.nav_experience}
+                                href="#experience-id"
+                            >
+                                <p>02.</p> Experience
+                            </a>
+                        </li>
+                        <li>
+                            <a className={styles.nav_work} href="#work-id">
+                                <p>03.</p> Work
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                className={styles.nav_contact}
+                                href="#contact-id"
+                            >
+                                <p>04.</p> Contact
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <div className={styles.menu}>
+                    <ul>
+                        <li>
+                            <a className={styles.nav_about} href="#about-id">
+                                <div>
+                                    <h1>01.</h1>
+                                    <h2>About</h2>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                className={styles.nav_experience}
+                                href="#experience-id"
+                            >
+                                <div>
+                                    <h1>02.</h1>
+                                    <h2>Experience</h2>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a className={styles.nav_work} href="#work-id">
+                                <div>
+                                    <h1>03.</h1>
+                                    <h2>Work</h2>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                className={styles.nav_contact}
+                                href="#contact-id"
+                            >
+                                <div>
+                                    <h1>04.</h1>
+                                    <h2>Contact</h2>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </header>
+    );
+}
