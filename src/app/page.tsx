e835ef993529876a4e074905c684c1f1f866f521
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-28 19:36:36 +0200

    about added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Introduction from '@/components/Introduction';
+import About from '@/components/About';
 import styles from './page.module.css';
 
 export default function Home() {
     return (
         <div className={styles.content}>
             <Introduction />
+            <About />
         </div>
     );
 }
---
+++ src/components/About/About.tsx
@@ -0,0 +1,30 @@
+import styles from './About.module.css';
+import Image from 'next/image';
+
+export default function About() {
+    return (
+        <div id="about-id" className={styles.about}>
+            <div className={styles.about_text}>
+                <div className={styles.about_init}>
+                    <div>
+                        <h2>01. </h2>
+                        <h1>About Me</h1>
+                    </div>
+                    <div className={styles.separator}></div>
+                </div>
+                <p>
+                    {`Hello! My name is Brayan and I'm a computer science student
+                    at Harbour.Space University. Lover of competitive
+                    programming and mathematics. Contestant in several
+                    international programming competitions obtaining medals and
+                    good results. Creator of various problems on platforms such
+                    as Codeforces. Enjoying teaching other people about
+                    programming and algorithms, as well as having a great
+                    passion for learning new things in the art of computer
+                    programming.`}
+                </p>
+            </div>
+            <Image src="/picture.jpg" alt="picture" width={300} height={300} />
+        </div>
+    );
+}
