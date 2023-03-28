a230bb99148bde18fc7c39cb847c2c2d5ce229fe
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-28 18:01:19 +0200

    layout added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import styles from './layout.module.css';
 
 export const metadata = {
     title: 'Personal Website',
@@ -12,7 +13,50 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                <header className={styles.header}>
+                    <section className={styles.wrapper}>
+                        <button className={styles.main_button}>B</button>
+                        <nav className={styles.navigation}>
+                            <ul>
+                                <li>
+                                    <a
+                                        className={styles.nav_about}
+                                        href="#about-id"
+                                    >
+                                        <p>01.</p> About
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        className={styles.nav_experience}
+                                        href="#experience-id"
+                                    >
+                                        <p>02.</p> Experience
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        className={styles.nav_work}
+                                        href="#work-id"
+                                    >
+                                        <p>03.</p> Work
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        className={styles.nav_contact}
+                                        href="#contact-id"
+                                    >
+                                        <p>04.</p> Contact
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </section>
+                </header>
+                {children}
+            </body>
         </html>
     );
 }
