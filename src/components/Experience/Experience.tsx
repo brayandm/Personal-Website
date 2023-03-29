ca989052426b4a5e4105560f64bc453e345be088
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-29 18:48:55 +0200

    some style added

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,5 +1,17 @@
 import styles from './Experience.module.css';
 
 export default function About() {
-    return <></>;
+    return (
+        <div id="experience-id" className={styles.experience}>
+            <div className={styles.experience_text}>
+                <div className={styles.experience_init}>
+                    <div>
+                        <h2>02. </h2>
+                        <h1>{`Where I've Worked`}</h1>
+                    </div>
+                    <div className={styles.separator}></div>
+                </div>
+            </div>
+        </div>
+    );
 }
