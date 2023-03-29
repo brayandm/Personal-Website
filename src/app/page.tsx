6bcdd1e1433ebc40ecefda605795c29f043ffab0
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-29 18:42:59 +0200

    experience added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Introduction from '@/components/Introduction';
 import About from '@/components/About';
 import styles from './page.module.css';
+import Experience from '@/components/Experience';
 
 export default function Home() {
     return (
         <div className={styles.content}>
             <Introduction />
             <About />
+            <Experience />
         </div>
     );
 }
---
+++ src/components/Experience/Experience.tsx
@@ -0,0 +1,5 @@
+import styles from './Experience.module.css';
+
+export default function About() {
+    return <></>;
+}
