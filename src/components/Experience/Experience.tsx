17732585d416d96d15a1aa5dbf2fad6e07f24d9b
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-29 19:07:37 +0200

    buttons added

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,6 +1,24 @@
-import styles from './Experience.module.css';
+'use client';
 
-export default function About() {
+import { ExperiencesQuery } from '@/generated/graphql';
+import styles from './Experience.module.css';
+import { sdk } from '@/graphql/client';
+import { useState } from 'react';
+
+export default function Experience() {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [experiences, setExperiences] = useState<
+        ExperiencesQuery | undefined
+    >();
+
+    if (!isLoaded) {
+        sdk.experiences().then((res) => {
+            setExperiences(res);
+            setIsLoaded(true);
+        });
+        return <></>;
+    }
+
     return (
         <div id="experience-id" className={styles.experience}>
             <div className={styles.experience_text}>
@@ -10,6 +28,19 @@ export default function About() {
                         <h1>{`Where I've Worked`}</h1>
                     </div>
                     <div className={styles.separator}></div>
+                </div>
+            </div>
+            <div className={styles.experience_list}>
+                <div className={styles.experience_buttons}>
+                    {experiences?.experiences.map((experience, index) => (
+                        <button
+                            key={index}
+                            id="experience-button-backend"
+                            className={styles.experience_button}
+                        >
+                            {experience.title}
+                        </button>
+                    ))}
                 </div>
             </div>
         </div>
