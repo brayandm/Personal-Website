44fc538c4ac0e560ce0da0ef64ebc614eaccc226
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-29 19:53:55 +0200

    arrow added

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -4,6 +4,7 @@ import { ExperiencesQuery } from '@/generated/graphql';
 import styles from './Experience.module.css';
 import { sdk } from '@/graphql/client';
 import { useState } from 'react';
+import Image from 'next/image';
 
 export default function Experience() {
     const [isLoaded, setIsLoaded] = useState(false);
@@ -22,85 +23,98 @@ export default function Experience() {
 
     return (
         <div id="experience-id" className={styles.experience}>
-            <div className={styles.experience_text}>
-                <div className={styles.experience_init}>
-                    <div>
-                        <h2>02. </h2>
-                        <h1>{`Where I've Worked`}</h1>
+            <div className={styles.experience_container}>
+                <div className={styles.experience_text}>
+                    <div className={styles.experience_init}>
+                        <div>
+                            <h2>02. </h2>
+                            <h1>{`Where I've Worked`}</h1>
+                        </div>
+                        <div className={styles.separator}></div>
                     </div>
-                    <div className={styles.separator}></div>
                 </div>
-            </div>
-            <div className={styles.experience_list}>
-                <div className={styles.experience_buttons}>
-                    {experiences?.experiences.map((experience, index) => (
-                        <button
-                            key={index}
-                            id="experience-button-backend"
-                            className={styles.experience_button}
-                            onClick={() => setSelectedExperience(index)}
-                        >
-                            {experience.title}
-                        </button>
-                    ))}
-                </div>
-                <div className={styles.experience_description}>
-                    <div className={styles.experience_description_section}>
-                        <div className={styles.title}>
-                            <h1
+                <div className={styles.experience_list}>
+                    <div className={styles.experience_buttons}>
+                        {experiences?.experiences.map((experience, index) => (
+                            <button
+                                key={index}
+                                id="experience-button-backend"
+                                className={styles.experience_button}
+                                onClick={() => setSelectedExperience(index)}
+                            >
+                                {experience.title}
+                            </button>
+                        ))}
+                    </div>
+                    <div className={styles.experience_description}>
+                        <div className={styles.experience_description_section}>
+                            <div className={styles.title}>
+                                <h1
+                                    className={
+                                        styles.experience_description_section_text
+                                    }
+                                >
+                                    {
+                                        experiences?.experiences[
+                                            selectedExperience
+                                        ].title
+                                    }
+                                </h1>
+                                <a
+                                    href={
+                                        experiences?.experiences[
+                                            selectedExperience
+                                        ].companyUrl
+                                    }
+                                    target="_blank"
+                                >
+                                    <h2
+                                        className={
+                                            styles.experience_description_section_company
+                                        }
+                                    >
+                                        @
+                                        {
+                                            experiences?.experiences[
+                                                selectedExperience
+                                            ].company
+                                        }
+                                    </h2>
+                                </a>
+                            </div>
+                            <h3
                                 className={
-                                    styles.experience_description_section_text
+                                    styles.experience_description_section_date
                                 }
                             >
                                 {
                                     experiences?.experiences[selectedExperience]
-                                        .title
-                                }
-                            </h1>
-                            <a
-                                href={
+                                        .startAt
+                                }{' '}
+                                -{' '}
+                                {
                                     experiences?.experiences[selectedExperience]
-                                        .companyUrl
+                                        .endAt
                                 }
-                                target="_blank"
-                            >
-                                <h2
-                                    className={
-                                        styles.experience_description_section_company
-                                    }
-                                >
-                                    @
-                                    {
-                                        experiences?.experiences[
-                                            selectedExperience
-                                        ].company
-                                    }
-                                </h2>
-                            </a>
-                        </div>
-                        <h3
-                            className={
-                                styles.experience_description_section_date
-                            }
-                        >
-                            {
-                                experiences?.experiences[selectedExperience]
-                                    .startAt
-                            }{' '}
-                            -{' '}
-                            {experiences?.experiences[selectedExperience].endAt}
-                        </h3>
-                        <div className={styles.experience_description_list}>
-                            {experiences?.experiences[
-                                selectedExperience
-                            ].tasks.map((task, index) => (
-                                <div
-                                    key={index}
-                                    className={styles.experience_element}
-                                >
-                                    <p>{task}</p>
-                                </div>
-                            ))}
+                            </h3>
+                            <div className={styles.experience_description_list}>
+                                {experiences?.experiences[
+                                    selectedExperience
+                                ].tasks.map((task, index) => (
+                                    <div
+                                        key={index}
+                                        className={styles.experience_element}
+                                    >
+                                        <Image
+                                            src={'/arrow.png'}
+                                            alt={'arrow'}
+                                            width={15}
+                                            height={15}
+                                        />
+                                        <p>{task}</p>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
