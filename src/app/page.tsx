f99760d1ce183298a0762ee070b55882dcd24840
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-30 19:03:59 +0200

    projects added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Introduction from '@/components/Introduction';
 import About from '@/components/About';
 import styles from './page.module.css';
 import Experience from '@/components/Experience';
+import Projects from '@/components/Projects';
 import { sdk } from '@/graphql/client';
 
 export default async function Home() {
@@ -13,6 +14,7 @@ export default async function Home() {
             <Introduction />
             <About />
             <Experience experiences={experiences} />
+            <Projects projects={projects} />
         </div>
     );
 }
---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,10 @@
+import { ProjectsQuery } from '@/generated/graphql';
+import styles from './Projects.module.css';
+
+interface Props {
+    projects: ProjectsQuery;
+}
+
+export default function Projects({ projects }: Props) {
+    return <></>;
+}
