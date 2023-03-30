a2cfb515f12b5b404f136a8a8bb1d9d53064c25a
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-03-30 20:24:16 +0200

    contact added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import About from '@/components/About';
 import styles from './page.module.css';
 import Experience from '@/components/Experience';
 import Projects from '@/components/Projects';
+import Contact from '@/components/Contact';
 import { sdk } from '@/graphql/client';
 
 export default async function Home() {
@@ -15,6 +16,7 @@ export default async function Home() {
             <About />
             <Experience experiences={experiences} />
             <Projects projects={projects} />
+            <Contact />
         </div>
     );
 }
---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,6 @@
+import styles from './Contact.module.css';
+import Image from 'next/image';
+
+export default function Contact() {
+    return <></>;
+}
